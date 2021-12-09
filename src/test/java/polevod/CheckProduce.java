6b856da9483a289907f7242bf89b8fca049a018d
olgayudicheva <olgayudicheva@gmail.com>
2021-12-09 14:26:39 +0300

    Проверка продуктов против угроз. Супер тест)) Для выполнения теста необходимо: - зарегистрированный пользователь в полеводе; - проведенные обходы с обнаруженными угрозами;

--- src/test/java/polevod/CheckProduce.java
+++ src/test/java/polevod/CheckProduce.java
@@ -1,5 +1,8 @@
 package polevod;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.squareup.okhttp.*;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -12,8 +15,9 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.IOException;
+import java.sql.Array;
+import java.util.*;
 
 
 public class CheckProduce {
@@ -32,15 +36,18 @@ public class CheckProduce {
     By bySelectTestovoeKhoz = new By.ByXPath("(//*[@class=\"ant-menu-item-group\"]//*[contains(.,'Тестовое хозяйство')])[last()]/../..");
     By byClickPlus = new By.ByXPath("//*[@id=\"app\"]/section/section/main/section/div[3]/div/div/div/div/div/table/tbody/tr[1]/td[1]/span");
 
-    By byCards = new By.ByXPath("(//div[contains(.,\"Снежная плесень\")])[last()]/../../..//div[@class=\"ExpandedRowItem_ProtectionWrapper_210X_\"]/div");
+    By threatCard = new By.ByXPath("//div[@class=\"ExpandedRowItem_Wrapper_25SVV \"]");
 
-    By byLoadNext = new By.ByXPath("(//div[contains(.,\"Снежная плесень\")])[last()]/../../..//span[contains(text(), \"Загрузить еще\")]/..");
+    By byCards = new By.ByXPath(".//div[@class=\"ExpandedRowItem_ProtectionWrapper_210X_\"]/div");
+
+    By byLoadNext = new By.ByXPath(".//span[contains(text(), \"Загрузить еще\")]/..");
 
     By getXpathByText(String text) {
-        return new By.ByXPath("(//*[contains(.,'"+text+"')])[last()]");
+        return new By.ByXPath("(//*[contains(.,'" + text + "')])[last()]");
     }
 
-    @Test //простой тест авторизации
+    @Test
+        //простой тест авторизации
     void testFirst() throws InterruptedException {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--start-fullscreen");
@@ -49,6 +56,8 @@ public class CheckProduce {
         //options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36");
         // options.addArguments();
         WebDriver driver = new ChromeDriver(options);
+        HashSet<String> errorDeveloper = new HashSet<>();
+        HashMap<String, ErrorUtl> errorNotFound = new HashMap<>();
         //driver.manage().window().maximize();
         try {
             driver.get("https://polevod.direct.farm/");
@@ -56,7 +65,7 @@ public class CheckProduce {
             Actions actions = new Actions(driver);
             wait.until((ExpectedConditions.visibilityOfElementLocated(phoneXpath)));
             WebElement phone = driver.findElement(phoneXpath);
-            WebElement pass = driver.findElement (new By.ByXPath("//input[@type=\"password\"]"));
+            WebElement pass = driver.findElement(new By.ByXPath("//input[@type=\"password\"]"));
             WebElement loginbutton = driver.findElement((new By.ByXPath("//button[@type=\"submit\"]")));
             phone.click();
             phone.sendKeys("9612884689");
@@ -78,78 +87,122 @@ public class CheckProduce {
             //
 
 
+            wait.until(ExpectedConditions.visibilityOfElementLocated(threatCard));
+            List<WebElement> threadCardList = driver.findElements(threatCard);
 
-            boolean end = false;
-            while (end==false) {
-                try {
-                    wait.until(ExpectedConditions.visibilityOfElementLocated(byLoadNext));
-                    wait.until(ExpectedConditions.elementToBeClickable(byLoadNext)).click();
-                } catch (Throwable t) {
-                    end = true;
-                }
-            }
 
-            wait.until(ExpectedConditions.visibilityOfElementLocated(byCards));
-            List<WebElement> cardsList = driver.findElements(byCards);
-            ArrayList<String> errorDeveloper = new ArrayList<>();
-            ArrayList<String> errorNotFound = new ArrayList<>();
-            LOGGER.info(cardsList.size());
-
-            for (int i=0;i<cardsList.size();i++) {
-                WebElement element = cardsList.get(i);
-                String name = element.findElement(new By.ByXPath(".//div[@class=\"ProtectionItem_ProtectionItemName_1o_qj\"]/a")).getText();
-                String developer = element.findElement(new By.ByXPath(".//div[@class=\"ProtectionItem_ProtectionItemRegistrant_RK9gY\"]")).getText();
-                //LOGGER.info(developer);
-                if (developer.isBlank()) {
-                    errorDeveloper.add("\""+name+"\"");
+            LOGGER.info("Кол угроз: "+threadCardList.size());
+            for (int threatCardIndex = 0; threatCardIndex < threadCardList.size(); threatCardIndex++) {
+                WebElement threadCard = threadCardList.get(threatCardIndex);
+                if (threadCard.findElements(new By.ByXPath(".//div[@class=\"ExpandedRowItem_ProtectionWrapper_210X_\"]/div[@class=\"ExpandedRowItem_ProtectionsPlaceholder_nUvki\"]")).size()>0) {
+                    continue;
                 }
 
-                By byDetails = new By.ByXPath(".//div[@class=\"ProtectionItem_ProtectionItemName_1o_qj\"]/a");
-                List<WebElement> detailsList = element.findElements(byDetails);
-                if (detailsList.isEmpty()) continue;
-                WebElement details = detailsList.get(0);
-                actions.moveToElement(details).perform();
-
-                wait.until(ExpectedConditions.visibilityOf(details));
-                wait.until(ExpectedConditions.elementToBeClickable(details)).click();
-                wait.until(ExpectedConditions.numberOfWindowsToBe(2));
-                String[] handles = driver.getWindowHandles().toArray(new String[2]);
-                driver.switchTo().window(handles[handles.length-1]);
-                wait.until(ExpectedConditions.presenceOfElementLocated(new By.ByTagName("header")));
-                List<WebElement> notFound404 = driver.findElements(new By.ByXPath("*[contains(.,\"404\")]"));
-                if (!notFound404.isEmpty()) {
-                    errorNotFound.add("\""+name+"\"");
+                boolean end = false;
+                while (end == false) {
+                    wait.until(ExpectedConditions.visibilityOfElementLocated(new By.ByXPath(".//div[@class=\"ExpandedRowItem_ProtectionWrapper_210X_\"]")));
+                    List<WebElement> load = threadCard.findElements(byLoadNext);
+                    if (load.isEmpty()) {
+                        end = true;
+                        break;
+                    }
+                    wait.until(ExpectedConditions.elementToBeClickable(load.get(0))).click();
                 }
-                driver.close();
-                driver.switchTo().window(handles[0]);
-            }
 
-            LOGGER.info("Пустые производители у "+errorDeveloper);
-            LOGGER.info("404: "+errorNotFound);
+                List<WebElement> cardsList = threadCard.findElements(byCards);
+                LOGGER.info(cardsList.size());
 
 
 
-            //actions = new Actions(driver);
-            //actions.moveToElement(driver.findElement(bySelectTestovoeKhoz)).perform();
-            //Thread.sleep(1000);
-            /*
-            wait.until((ExpectedConditions.visibilityOfElementLocated(bySelectKhoz)));
-            actions.moveToElement(driver.findElement(bySelectKhoz)).perform();
-            wait.until(ExpectedConditions.visibilityOfElementLocated(bySelectTestovoeKhoz));
-            actions.moveToElement(driver.findElement(bySelectTestovoeKhoz)).perform();
-            driver.findElement(bySelectTestovoeKhoz).click();*/
+                for (int i = 0; i < cardsList.size(); i++) {
+                    WebElement element = cardsList.get(i);
 
-            //открываем страницу "угрозы по культурам"
-            /*wait.until((ExpectedConditions.visibilityOfElementLocated(byArhive)));
-            driver.findElement(byArhive).click();*/
-
-
-
-        } finally {
-            Thread.sleep(10000);
-            driver.quit();
+                    WebElement nameElement = element.findElement(new By.ByXPath(".//div[@class=\"ProtectionItem_ProtectionItemName_1o_qj\"]/a"));
+                    String name = nameElement.getText();
+                    String href = nameElement.getAttribute("href");
+                    String developer = element.findElement(new By.ByXPath(".//div[@class=\"ProtectionItem_ProtectionItemRegistrant_RK9gY\"]")).getText();
+                    //LOGGER.info(developer);
+                    if (developer.isBlank()) {
+                        errorDeveloper.add(name);
                     }
 
+                    By byDetails = new By.ByXPath(".//div[@class=\"ProtectionItem_ProtectionItemName_1o_qj\"]/a");
+                    List<WebElement> detailsList = element.findElements(byDetails);
+                    if (detailsList.isEmpty()) continue;
+                    WebElement details = detailsList.get(0);
+                    actions.moveToElement(details).perform();
 
+                    wait.until(ExpectedConditions.visibilityOf(details));
+                    wait.until(ExpectedConditions.elementToBeClickable(details)).click();
+                    wait.until(ExpectedConditions.numberOfWindowsToBe(2));
+                    String[] handles = driver.getWindowHandles().toArray(new String[2]);
+                    driver.switchTo().window(handles[handles.length - 1]);
+                    wait.until(ExpectedConditions.presenceOfElementLocated(new By.ByTagName("header")));
+                    List<WebElement> notFound404 = driver.findElements(new By.ByXPath("*[contains(.,\"Ошибка 404\")]"));
+                    if (!notFound404.isEmpty()) {
+                        String okUrl = getUrlByName(name);
+                        errorNotFound.put(name, new ErrorUtl(okUrl, href));
+                    }
+                    driver.close();
+                    driver.switchTo().window(handles[0]);
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            Gson gson = new Gson().newBuilder().create();
+            LOGGER.info("Пустые производители у: " + gson.toJson(errorDeveloper));
+            LOGGER.info("Error url: " + gson.toJson(errorNotFound));
+            Thread.sleep(5000);
+            driver.quit();
+        }
+
+
+    }
+
+    class ErrorNotFound {
+        Set<String> errorDeveloper;
+
+    }
+
+    String getUrlByName(String name) throws IOException {
+        Gson gson = new Gson().newBuilder().create();
+        OkHttpClient okHttpClient = new OkHttpClient();
+
+
+        HttpUrl.Builder httpBuilder = HttpUrl.parse("https://direct.farm/api/resource/v2/knowledge/plant/search").newBuilder();
+        httpBuilder.addQueryParameter("query",name);
+        Request request = new Request.Builder()
+                .url(httpBuilder.build())
+                .build();
+
+        ResponseBody response = okHttpClient.newCall(request).execute().body();
+        JsonObject jsonObject = gson.fromJson(response.string(), JsonObject.class);
+        JsonObject first = jsonObject.getAsJsonArray("groups")
+                .get(0)
+                .getAsJsonObject()
+                .getAsJsonObject("articles")
+                .getAsJsonArray("objectList")
+                .get(0).getAsJsonObject();
+
+        JsonObject promotion = first.get("promotion").getAsJsonObject().get("path").getAsJsonObject();
+        int id = promotion.get("articleId").getAsInt();
+        String rootName = promotion.get("root").getAsString();
+        String group = promotion.get("group").getAsString();
+
+        String url = "https://direct.farm/knowledge/"+rootName+"/"+group+"/"+id;
+        LOGGER.info(url);
+        return url;
+    }
+
+    class ErrorUtl {
+        String ok;
+        String error;
+
+        public ErrorUtl(String ok, String error) {
+            this.ok = ok;
+            this.error = error;
+        }
     }
 }
