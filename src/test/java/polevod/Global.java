0e057ab9d728c23c70b5e0c716fc09239e2c10c5
Olgayudicheva <59446057+Olgayudicheva@users.noreply.github.com>
2023-10-24 16:19:35 +0300

    Update Global.java
    добавила комментарии по функциям

--- src/test/java/polevod/Global.java
+++ src/test/java/polevod/Global.java
@@ -3,10 +3,11 @@ package polevod;
 import org.openqa.selenium.By;
 
 abstract public class Global {
+    //поиск элементов в левом меню по тексту
     public static By findLeftMenuItemByText(String text) {
         return new By.ByXPath("(//*[contains(@class, \"ant-layout-sider-children\")]//*[contains(.,\""+text+"\")])[last()]/../../..");
     }
-
+    //поиск элементов по содержащимуся в нем тексту
     public static By findElementByContainsText(String text) {
         return new By.ByXPath("(//*[contains(.,'"+text+"')])[last()]");
     }
